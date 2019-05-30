2377fbff845f8431e9f664e40fa13aa4d5bc943f
cyj <chenyijie16@126.com>
2019-05-30 23:30:02 +0800

    change project to pure java

--- src/main/java/common/Tokens.java
+++
@@ -1,9 +0,0 @@
-package common;
-
-public class Tokens {
-    public static final String INT = "INT";
-    public static final String LONG = "LONG";
-    public static final String FLOAT = "LONG";
-    public static final String DOUBLE = "DOUBLE";
-    public static final String STRING = "STRING";
-}
--- src/main/java/server/Server.java
+++
@@ -1,9 +0,0 @@
-package server;
-
-public class Server {
-    public static void main(String[] args) {
-        System.out.println("server is running!");
-        System.out.println(System.getProperty("user.dir"));
-
-    }
-}
