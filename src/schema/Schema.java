ca067836861d073cd14c8a8ac8d62005bb87f606
cyj <chenyijie16@126.com>
2019-06-04 23:55:22 +0800

    bug fixes in schema manager

---
+++ src/schema/Schema.java
@@ -0,0 +1,99 @@
+package schema;
+
+import database.DataBase;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Schema {
+    private static Schema schema = new Schema();
+
+
+    private static final String METADATA_FILE = "schema";
+    private static final String DEFAULT_DATABASE = "test";
+    private List<DataBase> dataBases;
+
+    private Schema() {
+        dataBases = new ArrayList<>();
+        FileInputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(METADATA_FILE);
+        } catch (FileNotFoundException e) {
+            System.out.println("initializing default database schema");
+
+        } finally {
+            try {
+                if (inputStream != null) {
+                    DataInputStream in = new DataInputStream(inputStream);
+                    int dataBaseNumber = in.readInt();
+                    for (int i = 0; i < dataBaseNumber; i++) {
+                        int nameLength = in.readInt();
+                        byte[] nameBytes = new byte[nameLength];
+                        in.read(nameBytes);
+                        dataBases.add(DataBase.readFromFile(new String(nameBytes)));
+                    }
+                } else {
+                    createDatabase(new DataBase(DEFAULT_DATABASE));
+                    updateSchema();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }
+
+    }
+
+    public static Schema getSchema() {
+        return schema;
+    }
+
+    private void updateSchema() throws IOException {
+        FileOutputStream outputStream = new FileOutputStream(METADATA_FILE);
+        DataOutputStream out = new DataOutputStream(outputStream);
+        out.writeInt(dataBases.size());
+        for (DataBase dataBase : dataBases) {
+            out.writeInt(dataBase.getName().length());
+            out.write(dataBase.getName().getBytes());
+        }
+        out.close();
+        outputStream.close();
+    }
+
+    public void createDatabase(DataBase dataBase) throws IOException, IllegalArgumentException {
+        for (DataBase db : dataBases) {
+            if (db.getName().equals(dataBase.getName())) {
+                throw new IllegalArgumentException("SQL Execute Error [create database]: Cannot create a database with the same name as an existing database");
+            }
+        }
+        dataBases.add(dataBase);
+        updateSchema();
+    }
+
+    public DataBase getDatabaseByName(String name) {
+        for (DataBase dataBase : dataBases) {
+            if (dataBase.getName().equals(name)) {
+                return dataBase;
+            }
+        }
+        return null;
+    }
+
+    public List<DataBase> getDataBases() {
+        return dataBases;
+    }
+
+    public DataBase getDefaultDatabase() {
+        return getDatabaseByName(DEFAULT_DATABASE);
+    }
+
+    /**
+     * 删除所有有关表和数据文件
+     *
+     * @throws IOException
+     */
+    public void dropAll() throws IOException {
+
+    }
+}
---
+++ src/schema/SchemaManager.java
@@ -0,0 +1,26 @@
+package schema;
+
+import database.DataBase;
+
+public class SchemaManager {
+    private int id;
+    private static int uid = 0;
+    private DataBase currentDataBase;
+
+    private SchemaManager(int id) {
+        this.id = id;
+        currentDataBase = Schema.getSchema().getDefaultDatabase();
+    }
+
+    public static SchemaManager getNewSchemaManger() {
+        uid++;
+        return new SchemaManager(uid);
+
+    }
+
+    public DataBase getCurrentDataBase() {
+        return currentDataBase;
+    }
+
+
+}
